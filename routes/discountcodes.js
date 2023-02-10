5d18fec3e4f870c52fecd381324e221ae81086b1
Calvin <calvin@calvinwhitehurst.com>
2023-02-10 09:31:53 -0700

    datbase capabilities added to discount page

--- routes/discountcodes.js
+++ routes/discountcodes.js
@@ -19,20 +19,46 @@ router.get('/adddiscountcode', isLoggedIn, (req, res) => {
   )
 })
 
-// router.post('/printshoes', isLoggedIn, (req, res) => {
-//   let quantity = req.body.quantity
-//   let color = req.body.color
-//   let code = req.body.code
-//   let name = req.body.name
-//   let size = req.body.size
-//   res.render('printshoes', {
-//     quantity: quantity,
-//     color: color,
-//     code: code,
-//     size: size,
-//     name: name,
-//     layout: false
+// router.post('/addDiscount', isLoggedIn, (req, res) => {
+//   let title = req.body.title
+//   let link = req.body.link
+//   let message = req.body.message
+//   let start_date = req.body.start_date
+//   let end_date = req.body.end_date
+//   let store = req.body.store 
+//   let active = req.body.active
+//   res.render('adddiscountcode', {
+//     title: title,
+//     link: link,
+//     message: message,
+//     start_date: start_date,
+//     end_date: end_date,
+//     store: store,
+//     active: active
 //   })
 // })
+
+router.post('/addDiscount', isLoggedIn, (req, res) => {
+    let title = req.body.title
+    let link = req.body.link
+    let message = req.body.message
+    let start_date = req.body.start_date
+    let end_date = req.body.end_date
+    let store = req.body.store 
+    let active = req.body.active ? 1 : 0
+    var post = {
+      title,
+      link,
+      message,
+      start_date,
+      end_date,
+      store,
+      active
+    };
+    console.log(post);
+    connection.query(queries.discountInsert, post, function(error, result) {
+      res.redirect('adddiscountcode');
+    });
+  });
 
 module.exports = router
